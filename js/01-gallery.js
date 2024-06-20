68c439703f4a1c85852bd7d2eca34346a2bb4e48
CosminTaranu <ctaranu83@gmail.com>
2024-06-20 10:38:48 +0300

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector ('.gallery');
+
+const createItemMarkUp = ({ preview, original, description }) => {
+    return `
+    <li class="gallery__list">
+    <a class="gallery__link" href="${original}"> <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+    </a>
+    </li>
+    `
+}
+
+const galleryMarkUp = galleryItems.map(createItemMarkUp).join('');
+galleryContainer.innerHTML = galleryMarkUp;
+
+const galleryImage = document.querySelectorAll(".gallery__image");
+
+galleryImage.forEach ( image => {
+    image.addEventListener ( 'click', event =>{
+        event.preventDefault();
+        const imageUrl=event.target.dateset.source;
+        console.log(imageUrl);
+    });
+});
+
+const openModal = (imageUrl) => {
+    const instance = basicLightbox.create(`
+        <img src="${imageUrl}" width="800" height="600">
+    `);
+    instance.show();
+};
+
+galleryContainer.addEventListener ('click', event =>{
+    event.preventDefault();
+    if(!event.target.classList.contains ('gallery__image'))
+        return;
+    const imageUrl = event.target.dataset.source;
+    openModal(imageUrl);
+})
+
 
 console.log(galleryItems);
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector ('.gallery');
 
+const createItemMarkUp = ({ preview, original, description }) => {
+    return `
+    <li class="gallery__list">
+    <a class="gallery__link" href="${original}"> <img class="gallery__image" src="${preview}"  alt="${description}" />
+    </a>
+    </li>
+    `
+}
+
+const galleryMarkUp = galleryItems.map(createItemMarkUp).join('');
+galleryContainer.innerHTML = galleryMarkUp;
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
 console.log(galleryItems);
